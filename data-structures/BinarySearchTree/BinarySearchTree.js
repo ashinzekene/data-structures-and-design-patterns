18e2592c7c9c6a65dd229fb1e4025a9a3c4b315f
ashinzekene <ashinzekene@gmail.com>
2018-10-09 20:41:50 +0100

    working on BST, remove, findMin and findMax

--- data-structures/BinarySearchTree/BinarySearchTree.js
+++ data-structures/BinarySearchTree/BinarySearchTree.js
@@ -58,6 +58,11 @@ module.exports = class BinarySearchTree {
     searchParent(this.head);
   }
 
+  /**
+   * Finds an element in the tree
+   * @param {Number} x The element to find in the tree
+   * @returns {BSTNode|Boolean} returns the found node containing the element or false
+   */
   find(x) {
     const element = BinarySearchTree.toNumber(x);
     if (!element) {
@@ -65,14 +70,9 @@ module.exports = class BinarySearchTree {
       return false;
     }
     let node = this.head;
-    /**
-     * Searching
-     * @param {BSTNode} node the element to search
-     * @returns {Boolean} True if element is present
-     */
     while (node) {
       if (node.element === element) {
-        return true;
+        return node;
       }
       if (node.element > element) {
         node = node.left;
@@ -81,5 +81,94 @@ module.exports = class BinarySearchTree {
       }
     }
     return false;
+  }
+
+  /**
+   * Checks if an element is present in a tree
+   * @param {Number} x The number to check if present in the tree
+   * @returns {Boolean} True if element is in tree
+   */
+  isPresent(x) {
+    return Boolean(this.find(x));
+  }
+
+  /**
+   * Removing a node from a tree
+   * @param {Number} data The number to remove
+   */
+  remove(data) {
+    /**
+     * Replaces nodes after removing a node from it
+     * @param {BSTNode} node the current node
+     * @param {Number} data the number to search for
+     * @return {Node} resulting node after removal operation
+     */
+    const removeNode = function (node, data) {
+      if (node === null) {
+        return null;
+      }
+      if (node.element === data) {
+        if (!node.right && !node.left) {
+          return null;
+        }
+        if (!node.left) {
+          return node.right;
+        }
+        if (!node.right) {
+          return node.left;
+        }
+        if (node.left && node.right) {
+          let tempNode = node.right;
+          while (tempNode.left) {
+            tempNode = tempNode.left;
+          }
+          node.element = tempNode.element;
+          tempNode.right = node.right;
+        }
+      }
+      if (node.element > data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      }
+      if (node.element < data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+    this.head = removeNode(this.head, data);
+  }
+
+  /**
+   * Find the node with the largest value
+   * @returns {BSTNode} the element with the largest value
+   */
+  findMax() {
+    let node = this.head;
+    if (!node) {
+      return null;
+    }
+    while (node.right) {
+      node = node.right;
+    }
+    return node;
+  }
+
+  /**
+   * Find the node with the smallest value
+   * @returns {BSTNode} the element with the smallest value
+   */
+  findMin() {
+    let node = this.head;
+    if (!node) {
+      return null;
+    }
+    while (node.left) {
+      node = node.left;
+    }
+    return node;
+  }
+
+  emtpy() {
+    this.head = null;
   }
 };
--- test/binary-search-tree.js
+++ test/binary-search-tree.js
@@ -5,22 +5,42 @@ const BST = require('../data-structures/BinarySearchTree/BinarySearchTree');
 const bst = new BST('test-BST');
 
 test.beforeEach(() => {
+  bst.emtpy();
   bst.insert(1);
   bst.insert(2);
   bst.insert(3);
   bst.insert(4);
+  bst.insert(12);
   bst.insert(5);
   bst.insert(17);
   bst.insert(27);
+  bst.insert(14);
+  bst.insert(32);
+  bst.insert(14);
+  bst.insert(17);
+  bst.insert(18);
+  bst.insert(21);
   bst.insert(14);
   bst.insert(23);
 });
 
 test('Binary Search Tree - insert, find', t => {
-  t.true(bst.find(1));
   t.true(bst.find(2));
   t.true(bst.find(3));
   t.true(bst.find(4));
-  t.true(bst.find(14));
+  t.false(bst.find(44));
   t.false(bst.find(19));
+});
+
+test('Binary Search Tree - isPresent, find', t => {
+  t.true(bst.find(2));
+  t.false(bst.find(19));
+  // Expect false from both
+  t.is(Boolean(bst.find(14)), bst.isPresent(14));
+  t.is(bst.find(30), bst.isPresent(30));
+});
+
+test('Binary Search Tree - findMin and findMax', t => {
+  t.is(bst.findMin().element, 1);
+  t.is(bst.findMax().element, 32);
 });
