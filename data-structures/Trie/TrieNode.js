61f0f8c177c8859443ef52a6ab113584777c037e
ashinzekene <ashinzekene@gmail.com>
2018-10-12 19:56:13 +0100

    starting out trie

---
+++ data-structures/Trie/TrieNode.js
@@ -0,0 +1,16 @@
+const isEnd = Symbol('is-end');
+
+module.exports = class TrieNode {
+  constructor() {
+    this.keys = new Map();
+    this[isEnd] = false;
+  }
+
+  get isEnd() {
+    return this[isEnd];
+  }
+
+  setEnd(end = false) {
+    this[isEnd] = end;
+  }
+};
